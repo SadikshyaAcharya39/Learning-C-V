3c2b146a5e24ce9189801c2faed0d5cc1d03dea6
Sadikshya Acharya <acharya.sadikshya33@gmail.com>
2024-09-04 20:20:08 +0545

    Initial Commit

---
+++ PrintText.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    
+// Print Text
+
+printf("Hello World!!!");
+printf("I am learning C and it is awesome!!!");
+return 0;
+
+}
---
+++ PrintTextInNewLine.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+
+// Print Text in New Line
+
+printf("Hello World\n");
+printf("This is first time I am executing a program in C\n");
+
+
+printf("Hello World\nI am learning C.");
+
+return 0;
+
+}
+
