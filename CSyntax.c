83fcff50d9cae55ea8c171d7545c95c88c30842f
Sadikshya Acharya <acharya.sadikshya33@gmail.com>
2024-09-04 20:09:47 +0545

    First Commit

---
+++ CSyntax.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+
+// Creates a horizontal tab
+ printf("I am learning C....\t....\t....");
+
+ // Insert a backslash character
+ printf("Using \\ backslash\n");
+
+ // Inserting double quote character
+
+ printf("Hello, my name is \"Sadikshya Acharya\"");
+ printf("I am learning \"C\"");
+
+ return 0;
+ 
+}
