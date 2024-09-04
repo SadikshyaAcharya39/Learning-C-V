82a7b6b164a6a402044c7af3dafa6b066282200d
Sadikshya Acharya <acharya.sadikshya33@gmail.com>
2024-09-04 20:08:07 +0545

    First Commit

---
+++ Comments.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    // Single Line Comment
+
+    
+    printf("This is a \"//\" single line comment...");
+
+    printf("\n");
+
+    /* Multi-Line Comment */
+
+    printf("This is a \"/* */\" multi line comment");
+    return 0;
+}
