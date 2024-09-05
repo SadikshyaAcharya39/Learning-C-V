a8e24bebc38f4365897205d0593dabad690c90ba
Sadikshya Acharya <acharya.sadikshya33@gmail.com>
2024-09-05 19:47:17 +0545

    First Commit

---
+++ CharDataTypes.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    // char myGrade = 'A';
+    // printf("%c", myGrade);
+
+    // char char1, char2, char3;
+    // char1 = 'A';
+    // char2 = 'A';
+    // char3 = 'B';
+    // printf("%c %c %c", char1, char2, char3);
+
+    char a = 65, b = 66, c = 67;
+    printf("%c\n\n", a);
+    printf("%c\n\n", b);
+    printf("%c\n\n", c);
+
+    return 0;
+}
