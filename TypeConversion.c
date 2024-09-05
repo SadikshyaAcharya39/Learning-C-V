37e9958648358bd3b32900b62ea9b99e9f98496d
Sadikshya Acharya <acharya.sadikshya33@gmail.com>
2024-09-05 22:34:05 +0545

    First Commit

---
+++ TypeConversion.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+
+    // Type Conversion
+    // int x = 5;
+    // int y = 2;
+    // int remainder = x / y;
+    // printf("%d", remainder);
+
+
+    // Implicit Conversion (Automatically)
+    // Explicit Conversion (Manually)
+
+    // float myFloat = 9;
+    // printf("%f", myFloat);  // Compiler automatically converts int into float in this case
+
+    float remainder0 = 5 / 2;
+    printf("%f\n\n", remainder0);  // Output = 2 
+
+    float remainder1 = (float) 5 / 2;
+    printf("%f\n\n", remainder1);
+
+    return 0;
+}
