445f3658c9fc322e359fe0063bd5e91bc3662f33
Sadikshya Acharya <acharya.sadikshya33@gmail.com>
2024-09-13 20:55:11 +0545

    First Commit

---
+++ MemorySizes.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    int myInt;
+    // float myFloat;
+    // double myDouble;
+    // char myChar;
+
+    printf("%d", sizeof(myInt));
+    printf("%lu", sizeof(myInt));
+
+
+    return 0;
+}
