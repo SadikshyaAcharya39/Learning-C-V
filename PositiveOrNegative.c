f3d137a8c6164779085f0ce4d70427d495de9cc4
Sadikshya Acharya <acharya.sadikshya33@gmail.com>
2024-09-13 21:06:33 +0545

    First Commit

---
+++ PositiveOrNegative.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    int myNum = 10;
+    if(myNum < 0){
+        printf("Negative Number !!!");
+    }
+    else if(myNum == 0){
+        printf("Equal to Zero !!!");
+    }
+    else {
+        printf("Positive Number !!!");
+    }
+    return 0;
+}
