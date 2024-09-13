12334c97f2bb55a5ae801f477e9e80a38ee7c724
Sadikshya Acharya <acharya.sadikshya33@gmail.com>
2024-09-13 20:52:44 +0545

    First Commit

---
+++ ManyStatements.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+int main(){
+
+    // Many Statements
+
+    // printf("Hello World!!!\n\n");
+    // printf("Have a good day\n\n");
+
+    // Double Quote
+
+    // printf("Hello, I\'m from \"Nepal\"\n\n");
+    // printf("I\'m learning C.\n C is awesome !!!\n\n");
+
+
+    // Horizontal Tab
+
+    // printf("Creating a \thorizontal tab.");
+
+    // Declaring variables
+    // int a = 100;
+    // int b;
+    // b = 100;
+    // printf("%d\n", a);
+    // printf("%d", b);
+
+    // int myInt = 20;
+    // float myFloat = 33.33;
+    // double myDouble = 333.3333;
+    // printf("Int Value is: %d\n\n", myInt);
+    // printf("Float Value is: %f\n\n", myFloat);
+    // printf("Double Value is: %.4f", myDouble);
+
+    // char myChar = 'A';
+    // printf("%c\n\n", myChar);
+
+    // char *myText = " C is fun to learn ";
+    // printf("%s", myText);
+
+    // int x = 3;
+    // int y = 5;
+    // int sum = x + y;
+    // printf("%d", sum);
+
+    char myText[] = "Hello";
+    printf("%s", myText);
+
+    return 0;
+}
+
